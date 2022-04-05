f466e71f55ed0442e666040e47d1174010dc6cf0
Yang Cheng <annacheng09@hotmail.com>
2022-04-04 18:02:50 -0700

    finished category-routes and tested

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,6 +6,12 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     product_id: {
       type: DataTypes.INTEGER,
       references: {
@@ -21,7 +27,7 @@ ProductTag.init(
         key: "id",
         unique: false,
       },
-    }
+    },
   },
   {
     sequelize,
